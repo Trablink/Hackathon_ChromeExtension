5fb9bd4fab50d1478e05fd33ec01e590adde30a0
Jason Goldman <106274489+Trablink@users.noreply.github.com>
2022-11-16 21:37:05 -0600

    added icon drop down functionality

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 // declare an empty object to catch key presses
 let keysPressed = {};
 // add event listener for key down that adds the key to the empty object with a value of true
+//Hide your page and show a fake with Left/Right
 document.addEventListener("keydown", (event) => {
   keysPressed[event.key] = true;
   console.log(keysPressed);
@@ -8,6 +9,15 @@ document.addEventListener("keydown", (event) => {
     console.log(`It works!!!`);
   }
 });
+//Return to your regularly scheduled webpage with Up/Down
+document.addEventListener("keydown", (event) => {
+  keysPressed[event.key] = true;
+  console.log(keysPressed);
+  if (keysPressed["ArrowUp"] && event.key == "ArrowDown") {
+    console.log(`It works again!!!`);
+  }
+});
+
 // add event listener for key up that removed the key from the empty object (only true when depresssed)
 document.addEventListener("keyup", (event) => {
   delete keysPressed[event.key];
