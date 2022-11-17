65555ff7d0bf893a42702c28023ecbc4d696cda0
Jason Goldman <106274489+Trablink@users.noreply.github.com>
2022-11-17 13:14:30 -0600

    still working....

--- background.js
+++ background.js
@@ -1,20 +1,6 @@
-// const DEFAULT_URL = "https://www.codesmith.io/";
-// const DEFAULT_OPTION = "hideAll";
-// const DEFAULT_HIDE = "command + P";
-// const DEFAULT_RESTORE = "command + O";
-
 let keysPressed = {};
-// document.addEventListener("keydown", (event) => {
-//   keysPressed[event.key] = true;
-//   console.log(keysPressed);
-//   console.log(keysPressed);
-//   if (keysPressed["ArrowLeft"] && event.key == "ArrowRight") {
-//     console.log(`it's working!!`);
-//     window.open("https://www.google.com", "_self");
-//   }
-// });
 
-this.addEventListener("keydown", () => {
+chrome.window.addEventListener("keydown", (event) => {
   keysPressed[event.key] = true;
   console.log(keysPressed);
   console.log(keysPressed);
